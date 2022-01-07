2e7786a654d15716b1b64a6a57d31d3435a79ccf
Loki <allil22@mail.wlu.edu>
2022-01-06 22:57:05 -0500

    i'm a fucking pussy piece of shit my mind is crap fuck me

--- main.py
+++ main.py
@@ -3,7 +3,15 @@ import math
 
 class NN:
     def __init__(self, layers, layersize, activationfxn, costfxn, optimizer):
-        return 0
+        self.layers=layers # integer
+        self.layersize=layersize # tuple
+        # activationfxn is a tuple of length 1 or length=layers, to allow for different activation function at each layer
+        if len(activationfxn) < len(layers):
+            self.activationfxn=activationfxn[0] # if not enough layers are entered, just use the first activation fxn entered
+        else:
+            self.activationfxn=activationfxn 
+        self.costfxn=costfxn # MSE for now
+        self.optimizer=optimizer # basic SGD for now
 
 class Node:
-    def __init__
+    def __init__(self, )
